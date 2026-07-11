8fc5675f281b2b45cf4b0b6fa45c4ad7602ebc14
Andy Guinder <andreabelen.guinder@gmail.com>
2026-07-11 03:11:39 -0300

    config todoan

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Mi Aplicación de Tareas',
+        short_name: 'To-Do List',
+        description: 'Una app genial para organizar mi día',
+
+        theme_color: '#0E58C4',   
+        background_color: '#ecf4ff', 
+        display: 'standalone',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable' 
+          }
+        ]
+      }
+    })
+  ]
 })
